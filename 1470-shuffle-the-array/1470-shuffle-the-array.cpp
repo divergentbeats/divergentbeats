1779f62a731239a925f94aba57c0121fe5a18f74
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-02-12 18:21:35 +0530

    Time: 4 ms (64.18%), Space: 13.4 MB (50.82%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans;
        
-        for(int i=0,j=n;i<n,j<(2*n);i++,j++)
+        for(int i=0,j=n;i<n && j<(2*n);i++,j++)
         {
             
                 ans.push_back(nums[i]);

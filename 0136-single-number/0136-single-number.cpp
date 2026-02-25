307ba8d5aba777deae9c9e3ce606907f32db71fb
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-02-25 19:13:07 +0530

    Time: 6 ms (19.57%), Space: 23.8 MB (11.03%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -4,17 +4,12 @@ public:
         unordered_map<int,int> mp;
         int n=0;
         for(int i=0;i<nums.size();i++)
-        {
             mp[nums[i]]++;
-            if(i==nums.size()-1)
+        for(int j=0;j<nums.size();j++)
             {
-                for(int j=0;j<nums.size();j++)
-                {
-                    if(mp[nums[j]]==1)
-                    n=nums[j];
-                }
+            if(mp[nums[j]]==1)
+            n=nums[j];
             }
-        }
         return n;
     }
 };

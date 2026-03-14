f17eb32464721cc54b43c9e3e9a0cb2c56d0786f
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-03-14 20:28:16 +0530

    Time: 0 ms (100%), Space: 11.5 MB (97.23%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end()); 
+        int i = 0, key = 0, j = nums.size() - 1;
+
+        while(key <= j)
+        {
+            if(nums[key] == 0)
+            {
+                swap(nums[i], nums[key]);
+                i++;
+                key++;
+            }
+            else if(nums[key] == 1)
+            {
+                key++;
+            }
+            else
+            {
+                swap(nums[key], nums[j]);
+                j--;
+            }
+        }
     }
 };

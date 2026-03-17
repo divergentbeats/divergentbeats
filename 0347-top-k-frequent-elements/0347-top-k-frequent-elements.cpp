0d699190cf2b23790cc6272e1e9ac9c8a5b742ff
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-03-17 22:57:45 +0530

    Time: 2 ms (57.89%), Space: 17.7 MB (97.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,27 +3,24 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> mp;
         vector<int> ans;
-        int max=0;
-        int freq=0;
-        int i=0;
+        int freq=1,max=0;
         for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-        }
+        mp[nums[i]]++;
         for(int i=0;i<k;i++)
         {
-        for(auto &p:mp)
-        {
-            if(p.second>max)
+            for(auto &p:mp)
             {
-                max=p.second;
-                freq=p.first;
+                if(p.second>max)
+                {
+                    max=p.second;
+                    freq=p.first;
+                }
             }
+            ans.push_back(freq);
+            mp.erase(freq);
+            max=0;
         }
-        ans.push_back(freq);
-        mp.erase(freq);
-        max=0;
-        }
-        return ans;  
+        return ans;
+        
     }
 };

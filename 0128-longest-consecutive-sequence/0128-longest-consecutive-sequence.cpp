a099c8b58309f8adfc48ac2d89d72d1bbf2611cb
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-03-22 19:29:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         unordered_map<int,int> mp;
-        int count=0, maxlen=0;
+        int count=0, max=0;
         for(int i=0;i<nums.size();i++)
         mp[nums[i]]++;
         for(auto &p:mp)
         {
+            int num=p.first;
             if(mp.find(p.first-1)==mp.end())
             {
-                int current=p.first;
+                int current=num;
                 count=1;
                 while(mp.find(current+1)!=mp.end())
                 {
                     count++;
                     current++;
                 }
-                if(count>maxlen)
-                maxlen=count;
+                if(count>max)
+                max=count;
+                count=0;
             }
         }
-        return maxlen;     
+        return max;     
     }
 };

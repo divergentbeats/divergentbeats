ba0ab7132fac96b601ec884d30b5e5c4c3d02e7f
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-03-24 07:14:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,10 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
         unordered_map<int,int> mp;
-        int current=0;
         for(int i=0;i<nums.size();i++)
         {
-            current=target-nums[i];
+           int current=target-nums[i];
             if(mp.find(current)!=mp.end())
             { 
                 return {mp[current],i};

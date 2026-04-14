b23bdfffc227f1124074891f7ebb3b0aa4f0fc47
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-04-14 14:56:12 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.67%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        int left=0, right=nums.size()-1, mid,t,k;
         vector<int> ans(2);
-        int i=0,j=nums.size()-1,mid,k,t;
-        while(i<=j)
+        while(left<=right)
         {
-            mid=(i+j)/2;
-            if(nums[mid]==target)
+            mid=(left+right)/2;
+            if(target==nums[mid])
             {
-                k=mid;
-                while(k >= 0 && nums[k] == target)
-                k--;
-                ans[0]=k+1;
                 t=mid;
-                while(t < nums.size() && nums[t] == target)
-                t++;
-                ans[1]=t-1;
+                while(t>0 && nums[t-1]==target)
+                t--;
+                k=mid;
+                while(k<nums.size()-1 && nums[k+1]==target)
+                k++;
+                ans[0]=t;
+                ans[1]=k;
                 return ans;
-
             }
-            else if(nums[mid]<target)
-            i=mid+1;
+            else if(target < nums[mid])
+            right = mid-1;
             else
-            j=mid-1;
-
+            left = mid+1;
         }
         ans[0]=-1;
         ans[1]=-1;

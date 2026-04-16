9449a57c92daccdace2455079ea488f19e17b56f
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-04-16 22:08:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long i=0;
-        long ans;
-        while(i*i <= x)
-        {
-            ans=i;
-            i++;
+        long left = 0, right = x;
+        int ans = 0;
+
+        while(left <= right) {
+            long mid = (left + right)/ 2;
+
+            if(mid <= x / mid) {
+                ans = mid;          
+                left = mid + 1;     
+            }
+            else {
+                right = mid - 1;    
+            }
         }
+
         return ans;
     }
 };

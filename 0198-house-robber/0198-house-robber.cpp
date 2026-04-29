53bae26de4935a5c895ffa15fae4727e7e688a75
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-04-29 19:41:18 +0530

    Time: 0 ms (100%), Space: 10.2 MB (75.33%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev1 = 0, prev2=0, temp=0;
+
+        for(int num:nums)
+        {
+            temp = max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = temp;
+        }
+        return prev1;
+        
+    }
+};

4cb9f8f15506e9bb9db0dd396984c0188c80d844
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-06-11 21:37:15 +0530

    Time: 0 ms (100%), Space: 9.7 MB (49.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> mp;
-        if(t.size()!=s.size())
+        unordered_map<char,int> mp;
+        if(s.size()!=t.size())
         return false;
         for(int i=0;i<s.size();i++)
-        {
-            mp[s[i]]++;
-        }
+        mp[s[i]]++;
         for(int i=0;i<t.size();i++)
-        { 
-            mp[t[i]]--;
-            if(mp[t[i]]==-1)
+        {
+            if(mp[t[i]]==0)
             return false;
+            mp[t[i]]--;
         }
-        return true;
+        return true; 
     }
 };

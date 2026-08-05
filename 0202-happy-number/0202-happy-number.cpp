ae2ee3410e5c554bd2656f69288bf16ed917317e
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-08-05 10:35:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (5.88%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    unordered_set<int> st;
+public:
+    bool isHappy(int n) {
+        
+        if(st.find(n)!=st.end())
+        return false;
+        st.insert(n);
+       int s = n, sum = 0;
+       while(s>0)
+       {
+        int x = s % 10;
+        int newn = pow(x,2);
+        sum+=newn;
+        s/=10;
+       }
+        
+       
+       if(sum==1)
+       return true;
+       
+       return isHappy(sum);
+        
+    }
+};

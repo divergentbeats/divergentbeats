79b99304d1c9bdcf08a80d969f5ecac2ca043b55
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-08-18 09:30:56 +0530

    Time: 4 ms (14.33%), Space: 9.8 MB (94.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left=0;
-        int right=s.size()-1;
-        while(left<right)
+        int i=0, j=s.size()-1;
+        while(i<j)
         {
-            if(!isalnum(s[left]))
-            left++;
-            else if(!isalnum(s[right]))
-            right--;
-            else if(tolower(s[left])!=tolower(s[right]))
+            if(!isalnum(s[i]))
+            i++;
+            else if(!isalnum(s[j]))
+            j--;
+            else if(tolower(s[i])!=tolower(s[j]))
             return false;
             else
-            left++, right--;
+            i++,j--;
         }
+
         return true;
         
     }

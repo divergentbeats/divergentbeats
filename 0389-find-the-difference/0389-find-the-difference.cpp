d12c6e3b0000e514a42ec0756db1fa9144ac9728
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-08-23 19:13:09 +0530

    Time: 1 ms (19.82%), Space: 9.4 MB (25.7%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         unordered_map<char,int> mp;
-        char ans;
         for(int i=0;i<s.size();i++)
         mp[s[i]]++;
         for(int i=0;i<t.size();i++)
-        {   
-            if(mp[t[i]]==0)
-            ans=t[i];
+        {
+            if(mp.find(t[i])==mp.end())
+            return t[i];
+            else if(mp[t[i]]==0)
+            return t[i];
             mp[t[i]]--;
         }
-  
-        return ans;
+        return NULL;
         
     }
 };

5797c2251818282cb8c34cf618735c5f3cc67c9e
Abhiram Bhat <69000590+divergentbeats@users.noreply.github.com>
2026-09-25 21:17:45 +0530

    Time: 0 ms (100%), Space: 15.2 MB (70.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,13 +4,24 @@ public:
         int left=0, right=nums.size()-1;
         while(left<=right)
         {
-            if(nums[left]==target)
-            return left;
-            else if(nums[right]==target)
-            return right;
-            left++,right--;
+            int mid=(left+right)/2;
+            if(nums[mid]==target)
+            return mid;
+            else if(nums[left]<=nums[mid])
+            {
+                if(nums[left]<=target && target<=nums[mid])
+                right=mid-1;
+                else
+                left=mid+1;
+            }
+            else if(nums[mid]<=nums[right])
+            {
+                if(nums[mid]<=target && target<=nums[right])
+                left=mid+1;
+                else
+                right=mid-1;     
+            }           
         }
-
-        return -1;   
+        return -1;
     }
 };
